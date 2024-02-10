a5dc4f71085ed4255d1b611f17662440ddf68c57
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2024-02-10 13:46:35 +0100

    third session

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Second session',
+        title: 'Third session',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: MyCalculator());
+        home: Container());
   }
 }
